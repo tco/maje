c93ce3b2f161aae0b00b14f5a1a03af3019a760a
Antti Rintala <antti.rintala@gmail.com>
2012-02-29 07:29:02 +0200

    Fetch majic from http or https if protocol matches

--- maje.js
+++ maje.js
@@ -8,30 +8,44 @@ var fs = require('fs'),
 var majeDescriptor = fs.readFileSync('maje.maje', 'utf8'),
     maje = JSON.parse(majeDescriptor),
     basePath = maje.paths.development,
-    buildTarget = 'development';
+    buildTarget = 'development',
+    supportedProtocols = ['http', 'https'];
 
-mkdirp(basePath, 0777, function(err) {
-    if(!err) {
+mkdirp(basePath, 0777, function(error) {
+    if(!error) {
         for(m in maje.majics) {
-            castMajic(maje.majics[m])
+            var majicSource = maje.majics[m],
+                protocol = majicSource.split(":").shift() || undefined;
+            //If online source go and fetch
+            if(supportedProtocols.indexOf(protocol) >= 0) {
+                request({ uri: majicSource}, function(error, response, majicDescriptor) {
+                    if(!error && response.statusCode == 200) {
+                        castMajic(JSON.parse(majicDescriptor));
+                    }
+                });
+            //Assume local source
+            } else {
+                var majicDescriptor = fs.readFileSync(majicSource),
+                    majic = JSON.parse(majicDescriptor);
+                castMajic(majic);
+            }
         }
     } else {
-        console.log(err);
+        console.log(error);
         process.exit(1);
     }
 });
 
-function castMajic(m) {
-    var majicDescriptor = fs.readFileSync(m),
-        majic = JSON.parse(majicDescriptor),
-        path = [basePath,majic.paths.local].join(""),
+function castMajic(majic) {
+    var path = [basePath,majic.paths.local].join(""),
         source = majic.source[buildTarget],
+        protocol = source.split(":").shift() || undefined,
         filename = source.split("/").pop();
-    mkdirp(path, 0777, function(err) {
-        if(!err) {
+    mkdirp(path, 0777, function(error) {
+        if(!error) {
             request({ uri: source }).pipe(filed([path,filename].join("")));
         } else {
-            console.log(err);
+            console.log(error);
             process.exit(1);
         }
     });
