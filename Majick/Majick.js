dffed72a10f7b3524899ba914c228964ad90aff1
Antti Rintala <antti.rintala@gmail.com>
2013-02-03 22:24:34 +0200

    Refactored to use Backbone Models and Collection

--- Majick/Majick.js
+++ Majick/Majick.js
@@ -1,50 +1,53 @@
-var Bacon = require("baconjs").Bacon,
+var Backbone = require('backbone'),
+    _ = require('underscore'),
+    Bacon = require("baconjs").Bacon,
     wrench = require('wrench'),
     io = require('socket.io-client'),
 
+    Metrics = Backbone.Collection.extend(),
+
     socket = io.connect('localhost', { port: 1337 }),
     majickBus = new Bacon.Bus(),
+    metrics = new Metrics(),
 
-    modules = {
-        timeouts: {},
-        replace: function(name, module) {
-            var stream = new Bacon.Bus();
-
-            majickBus.plug(stream);
-
-            clearTimeout(modules.timeouts[name]);
-            //TODO: hook this up with Backbone collection change
-            (function(name, module, stream) {
-                setTimeout(function streamMetric() {
-                    stream.push(module.metric);
-                    modules.timeouts[name] = setTimeout(streamMetric, 1000);
-                }, 0);
-            })(name, module, stream);
-
-            modules[name] = module;
-        },
-        getMetric: function(name) {
-            if(this[name]) {
-                return this[name].metric;
-            }
-            return undefined;
-        }
-    },
     loadModules = function loadModules() {
-        var files = wrench.readdirSyncRecursive("modules/");
+
+        var files = wrench.readdirSyncRecursive("metrics/");
 
         files.forEach(function(filename) {
+
             var extension = filename.substr(-3),
                 basename = filename.substr(0, filename.indexOf(extension));
+
             if(extension === '.js') {
-                var module = require('./modules/' + filename);
-                modules.replace(basename, module);
+
+                var metric = require('./metrics/' + filename).metric,
+                    old = _.first(metrics.where(function(m) { return m.get('name') == basename; }));
+                //var
+
+                if(old !== undefined) {
+                    metrics.remove(old);
+                }
+
+                metrics.add(metric);
+                metric.plugIn(majickBus);
+
             }
+
         });
+
     };
 //var
 
 loadModules();
+
+metrics.on('change', function(metric) {
+    var stream = metric.get('stream'),
+        name = metric.get('name'),
+        streamEvent = {};
+    streamEvent[name] = metric.get(name);
+    stream.push(streamEvent);
+});
 
 majickBus.onValue(function(value) {
     socket.emit('message', value);
---
+++ Majick/metrics/cpu.js
@@ -0,0 +1,31 @@
+var Backbone = require('backbone'),
+    Bacon = require("baconjs").Bacon,
+    cpuUsage = require('cpu-usage'),
+
+    interval = 1000,
+    Metric = Backbone.Model.extend({
+        initialize: function(name) {
+            this.set('interval', interval);
+            this.set('name', name);
+            this.set('stream', new Bacon.Bus());
+            this.bind("remove", function() {
+                this.destroy();
+            });
+        },
+
+        plugIn: function(stream) {
+            stream.plug(this.get('stream'));
+        }
+    }),
+    metric = new Metric('cpu');
+//var
+
+(function setMetric(metric) {
+    metric.set(metric.get('name'), { load: Math.random().toFixed(2) });
+    //metric.trigger('change');
+    setTimeout(function() {
+        setMetric(metric);
+    }, metric.get('interval'));
+})(metric);
+
+exports.metric = metric;
