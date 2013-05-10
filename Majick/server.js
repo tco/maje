7aca58a2bde5a6e6b2ebd8c69398437424803751
Antti Rintala <antti.rintala@gmail.com>
2013-05-10 07:49:05 +0300

    Maje

--- Majick/server.js
+++
@@ -1,7 +0,0 @@
-var io = require('socket.io').listen(1337);
-
-io.sockets.on('connection', function (socket) {
-    socket.on('message', function (data) {
-        console.log(data);
-    });
-});
---
+++ index.js
@@ -0,0 +1,16 @@
+// Resources
+var Bacon = require('baconjs').Bacon,
+    //Streamer = require('maje-streamer'),
+    Feature = require('maje-feature'),
+    Component = require('maje-component');
+
+
+//Maje
+
+var Maje = {
+    Feature: Feature,
+    Component: Component
+};
+
+
+module.exports = Maje;
